cabe42a3d4023b5c733ae724812061f41d2bd839
EnvasChan <envasenchan@gmail.com>
2021-02-10 21:10:15 +0900

    practice fibonacci and complete other algorithms

--- acm11057.cpp
+++ acm11057.cpp
@@ -23,13 +23,54 @@ using namespace std;
     return
   2 1 n
  
+0 0 3
+  0 1 3
+    0 2 3 -> return 10
+      0 3 3 -> return 1 
+      1 3 3 ->
+      2 3 3 ->
+      ...
+      9 3 3 
+    1 2 3
+      1 3 3
+      2 3 3
+      ...
+      9 3 3
+    2 2 3
+      2 3 3
+      3 3 3
+      ...
+      9 3 3
+
+  1 1 3
+    1 2 3
+      
+    2 2 3
+
+
+
+
 
 
 */
+
+
+
+
+int dp[10][1001] = {0};
+
+
+
 int solve(int prev, int current, int n)
 {
   // ...
   int sum = 0;
+  // 저장되어있는지 확인
+  // 꺼내기
+  if (dp[prev][n-current] != 0)
+  {
+    return dp[prev][n-current];
+  }
 
   if(current == n)
   {
@@ -40,7 +81,7 @@ int solve(int prev, int current, int n)
   {
     sum += solve(i, current+1, n);
   }
-
+  dp[prev][n-current] = sum;
   return sum;
   // ...
 }
@@ -49,6 +90,9 @@ int solve(int prev, int current, int n)
 
 int main()
 {
-
+    int n = 0;
+    cin >> n;
+    cout << solve(0,0,n);
+    return 0;
 
 }
---
+++ fibonacci.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+int dp[10000] = {0};
+
+// recursive (재귀)
+int fibonacci(int f = 0)
+{
+    if(dp[f] != 0)
+    {
+        return dp[f];
+    }
+
+    if(f == 0)
+    {
+        return 0;
+    }
+
+    if(f == 1)
+    {
+        return 1;
+    }
+
+    dp[f] = fibonacci(f -1) + fibonacci(f - 2);
+
+    return dp[f];
+}
+
+// non-recursive (재귀아님)
+int fibonacci2(int f)
+{
+    int fib[10000] = {0};
+    fib[1] = 1;
+    fib[2] = 1;
+    for (int i=3; i<=f; i++)
+    {
+        fib[i] = fib[i -1] + fib[i - 2];
+
+    }
+
+    return fib[f];
+}
+
+
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
